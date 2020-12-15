611232a5420697868cc872aa8b486773b71bfc61
Reto Gmür <reto@factsmission.com>
2020-12-15 19:14:25 +0100

    Revert "running script from setup dir"
    This reverts commit fc6a41fb20c89c1d0e50e0399b33c2104e2acf0e.

--- setup/native-host.js
+++ setup/native-host.js
@@ -14,7 +14,7 @@ const setup = new Setup({
   hostDescription: "Exposes an ssb-client to the scuttle shell browser extension",
   browser: "firefox",
   hostName: "scuttle_shell_browser",
-  mainScriptFile: "../host/host-script.js",
+  mainScriptFile: "host/host-script.js",
   chromeExtensionIds: ["chrome-extension://xxxxxx"],
   webExtensionIds: ["scuttle-shell-browser@example.org"],
   overwriteConfig: true,
