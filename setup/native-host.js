2dd6a7e809c4daf15c957ffcb91b3d59986b257b
Reto Gmür <reto@factsmission.com>
2020-12-18 18:48:31 +0100

    getting absolute path of script

--- setup/native-host.js
+++ setup/native-host.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import {Setup} from "web-ext-native-msg"
+import path from 'path'
 
 const handlerAfterSetup = info => {
   const {configDirPath, shellScriptPath, manifestPath} = info;
@@ -10,11 +11,17 @@ const handlerAfterSetup = info => {
   console.log('You can download the Add-On here: https://github.com/retog/scuttle-shell-browser/releases/tag/1.0.0')
 };
 
+const getMainScriptPath = () => {
+  const [, scriptPath] = process.argv;
+  const mainScriptPath = path.resolve(path.dirname(scriptPath), '../host/host-script.js');
+  return mainScriptPath;
+};
+
 const setup = new Setup({
   hostDescription: "Exposes an ssb-client to the scuttle shell browser extension",
   browser: "firefox",
   hostName: "scuttle_shell_browser",
-  mainScriptFile: "host/host-script.js",
+  mainScriptFile: getMainScriptPath(),
   chromeExtensionIds: ["chrome-extension://xxxxxx"],
   webExtensionIds: ["scuttle-shell-browser@example.org"],
   overwriteConfig: true,
